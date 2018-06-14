197547190c6026c60b1f2897c896f19c965bd330
oufen <1263759508@qq.com>
2018-06-14 16:36:52 +0800

    update: 添加.gitignore文件

--- TDFAPILoggerExample/Pods/Target Support Files/Pods-TDFAPILoggerExample/Pods-TDFAPILoggerExample-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TDFAPILoggerExample : NSObject
-@end
-@implementation PodsDummy_Pods_TDFAPILoggerExample
-@end
--- TDFAPILoggerExample/Pods/Target Support Files/Pods-TDFAPILoggerExampleTests/Pods-TDFAPILoggerExampleTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TDFAPILoggerExampleTests : NSObject
-@end
-@implementation PodsDummy_Pods_TDFAPILoggerExampleTests
-@end
