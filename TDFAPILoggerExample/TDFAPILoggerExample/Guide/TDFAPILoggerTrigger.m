78314db428641dc8949bd133fc974f960e1b3036
oufen <1263759508@qq.com>
2018-06-14 16:52:18 +0800

    update: 完善trigger类的注释

--- TDFAPILoggerExample/TDFAPILoggerExample/Guide/TDFAPILoggerTrigger.m
+++ TDFAPILoggerExample/TDFAPILoggerExample/Guide/TDFAPILoggerTrigger.m
@@ -8,25 +8,60 @@
 
 #import "TDFAPILoggerTrigger.h"
 #import "TDFAPILogger.h"
-//#import "TDFHTTPClient.h"
+#import <UIKit/UIKit.h>
 
 @implementation TDFAPILoggerTrigger
 
 + (void)load {
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-//        [TDFAPILogger sharedInstance].requestLoggerElements = TDFAPILoggerRequestElementHTTPBody;
-//        [TDFAPILogger sharedInstance].responseLoggerElements = TDFAPILoggerResponseElementResponse;
-
-//        [TDFAPILogger sharedInstance].loggerFilter = ^BOOL(__kindof NSURLRequest const *request) {
-//            return YES;
-//        };
-//        TDFAPILoggerRequestLogIcon = "⚽️";
-//        TDFAPILoggerResponseLogIcon = "🏀";
-//        TDFAPILoggerErrorLogIcon = "🏓";
-//        [TDFAPILogger sharedInstance].serverModuleWhiteList = @[@"customer_manager", @"income"];
-//        [TDFAPILogger sharedInstance].defaultTaskDescriptionObj = [TDFHTTPClient sharedInstance];
-//        [[TDFAPILogger sharedInstance] open];
+        __weak __typeof(NSNotificationCenter *) center = [NSNotificationCenter defaultCenter];
+        __block id observer = [[NSNotificationCenter defaultCenter] addObserverForName:UIApplicationDidFinishLaunchingNotification object:nil queue:nil usingBlock:^(NSNotification * _Nonnull note) {
+            // 这里开始定制API日志输出的元素组成
+            [TDFAPILogger sharedInstance].requestLoggerElements = \
+                            TDFAPILoggerRequestElementTakeOffTime |
+                            TDFAPILoggerRequestElementMethod |
+                            TDFAPILoggerRequestElementVaildURL |
+                            TDFAPILoggerRequestElementHeaderFields |
+                            TDFAPILoggerRequestElementHTTPBody |
+                            TDFAPILoggerRequestElementTaskIdentifier;
+            
+            [TDFAPILogger sharedInstance].responseLoggerElements = \
+                            TDFAPILoggerResponseElementLandTime |
+                            TDFAPILoggerResponseElementTimeConsuming |
+                            TDFAPILoggerResponseElementMethod |
+                            TDFAPILoggerResponseElementVaildURL |
+                            TDFAPILoggerResponseElementHeaderFields |
+                            TDFAPILoggerResponseElementStatusCode |
+                            TDFAPILoggerResponseElementResponse |
+                            TDFAPILoggerResponseElementTaskIdentifier;
+            
+            // 这里开始定制API日志筛选规则
+            [TDFAPILogger sharedInstance].loggerFilter = ^BOOL(__kindof NSURLRequest const *request) {
+                /*
+                 if (不满足输出的条件) {
+                     return NO;
+                 }
+                 */
+                return YES;
+            };
+            
+            // 这里开始定制API日志状态分割线的字符
+            TDFAPILoggerRequestLogIcon = "⚽️";
+            TDFAPILoggerResponseLogIcon = "🏀";
+            TDFAPILoggerErrorLogIcon = "🏓";
+            
+            // 这里开始添加请求URL的server path白名单，不在白名单的都会被屏蔽
+            [TDFAPILogger sharedInstance].serverModuleWhiteList = @[@"server_path1", @"server_path2"];
+            
+            // 这里开始设置每个API默认的taskDescription的拥有者
+            [TDFAPILogger sharedInstance].defaultTaskDescriptionObj = nil;
+            
+            // 这里开启API日志
+            [[TDFAPILogger sharedInstance] open];
+            
+            [center removeObserver:observer name:UIApplicationDidFinishLaunchingNotification object:nil];
+        }];
     });
 }
 
